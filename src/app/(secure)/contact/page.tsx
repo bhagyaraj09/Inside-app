50f983b4f650316b713e7a42a95657b59ea83fea
Ashok Cherukuri <ashok@symphonize.com>
2024-05-31 13:06:07 -0700

    Workplace Profesionalism

--- src/app/(secure)/contact/page.tsx
+++ src/app/(secure)/contact/page.tsx
@@ -1,12 +1,20 @@
 import Title from '@/components/ui/title'
 import Container from '@/components/ui/container'
+import {
+  Card,
+  CardContent,
+} from '@/components/ui/card'
 
 export default function Contact() {
   return (
     <>
       <Title title="Contact"></Title>
       <Container>
-        Coming soon ...
+        <Card>
+          <CardContent className="grid pt-6">
+
+          </CardContent>
+        </Card>
       </Container>
     </>
   )
