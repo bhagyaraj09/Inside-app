007f96cdfe8e57f5b8693775f4fc7e1e1b2f6f82
bhagyaraj09 <BhagyaRaj.Jujjavarapu@symphonize.com>
2024-09-04 03:39:37 -0600

    added console for checking

--- src/components/time/time-approval-form.tsx
+++ src/components/time/time-approval-form.tsx
@@ -64,7 +64,7 @@ export default function TimeApprovalForm(props: TimeFormProps) {
                             </td>
                             <td className="p-2 align-middle">
                                 <>
-                                {console.log(new Date(timesheet.date).toDateString(),  new Date().getTimezoneOffset())}
+                                {console.log(new Date(timesheet.date).toDateString(), new Date().getTimezoneOffset(),timesheet.date.toDateString())}
                                 </>
                                 
                                 <div className="w-36">{new Date(timesheet.date).toDateString()}</div>
