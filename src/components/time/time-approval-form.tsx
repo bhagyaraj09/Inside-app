4b23bcdcd8453a043566d09d6980bdf123a9995a
bhagyaraj09 <BhagyaRaj.Jujjavarapu@symphonize.com>
2024-09-04 03:49:16 -0600

    testing logic

--- src/components/time/time-approval-form.tsx
+++ src/components/time/time-approval-form.tsx
@@ -64,7 +64,7 @@ export default function TimeApprovalForm(props: TimeFormProps) {
                             </td>
                             <td className="p-2 align-middle">
                                 <>
-                                {console.log(new Date(timesheet.date).toDateString(), new Date().getTimezoneOffset(),timesheet.date,typeof timesheet.date)}
+                                {console.log(new Date(new Date(timesheet.date).getTime() - (new Date().getTimezoneOffset() * 60000)), new Date().getTimezoneOffset(), timesheet.date, typeof timesheet.date)}
                                 </>
                                 
                                 <div className="w-36">{new Date(timesheet.date).toDateString()}</div>
